c5c45f84d8854b2ba049c8c8d25cdc14436173e6
Vincent <v3rdugo@yahoo.com>
2022-08-23 21:37:52 -0700

    initial commit

---
+++ contracts/Auction.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+interface IERC721
+{
+    function transferFrom(
+        address from,
+        address to,
+        uint256 nftId
+    ) external;
+}
+
+contract Auction
+{
+    
+}
